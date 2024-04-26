61ae3a3d9391b9c3510e164befb39c17e814b0ac
kielGavanez <130537228+kielGavanez@users.noreply.github.com>
2024-04-26 21:27:09 +0800

    done for sample calendar

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -8,45 +8,46 @@
     
 </head>
 <body>
-    <h2>PHP Calendar</h2>
+<h2>PHP Calendar</h2>
 
-    <table>
-        <tr>
-            <th>Sun</th>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Web</th>
-            <th>Thu</th>
-            <th>Fri</th>
-            <th>Sat</th>
-        </tr>
-        <?php
-            $firstDayOfMonth = mktime(0, 0, 0, date("m"), 1, date("y"));
-            $daysOfMonth = date("t", $firstDayOfMonth);
-            $firstDayOfWeek = date("w", $firstDayOfMonth);
+    <div class="calendar-container">
+        <table>
+            <tr>
+                <th>Sun</th>
+                <th>Mon</th>
+                <th>Tue</th>
+                <th>Wed</th>
+                <th>Thu</th>
+                <th>Fri</th>
+                <th>Sat</th>
+            </tr>
+            <?php
+                $firstDayOfMonth = mktime(0, 0, 0, date("m"), 1, date("Y"));
+                $daysInMonth = date("t", $firstDayOfMonth);
+                $firstDayOfWeek = date("w", $firstDayOfMonth);
 
-            $currentDay = 1;
-            echo "<tr>";
-
-            for ($i=0; $i < 7; $i++) { 
-                if ($i < $firstDayOfWeek) {
-                    echo "<td></td>";
-                }else{
-                    echo "<td>$currentDay</td>";
-                    $currentDay++;
-                }
-            }
-            echo "</tr>";
-
-            while ($currentDay <= $daysOfMonth) {
+                $currentDay = 1;
                 echo "<tr>";
-                for ($i=0; $i < 7 && $currentDay <= $daysOfMonth; $i++) { 
-                    echo "<tr>$currentDay</tr>";
-                    $currentDay++;
+                for ($i = 0; $i < 7; $i++) {
+                    if ($i < $firstDayOfWeek) {
+                        echo "<td></td>";
+                    } else {
+                        echo "<td>$currentDay</td>";
+                        $currentDay++;
+                    }
                 }
                 echo "</tr>";
-            }
-        ?>
-    </table>
+
+                while ($currentDay <= $daysInMonth) {
+                    echo "<tr>";
+                    for ($i = 0; $i < 7 && $currentDay <= $daysInMonth; $i++) {
+                        echo "<td>$currentDay</td>";
+                        $currentDay++;
+                    }
+                    echo "</tr>";
+                }
+            ?>
+        </table>
+    </div>
 </body>
 </html>
